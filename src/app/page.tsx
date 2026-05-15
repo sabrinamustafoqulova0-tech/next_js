19f23321fa34b29c6ed3af40775847ee2be89abd
Sabrina Mustafoqulova <sabrinamustafoqulova0@gmail.com>
2026-05-15 17:42:38 +0500

    lesson8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
-'use client'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { useDeleteTodoMutation, useGettodoByNameQuery } from './services/todo.api'
-import { IUsers } from './services/todo.type'
+import React from 'react'
 
-export default function Navbar() {
-  const { data, error, isLoading } = useGettodoByNameQuery(null)
-  const[deleteUser, { data:data2, error:error2, isLoading:isLoading2  }] = useDeleteTodoMutation()
-  
+const HomePage = () => {
   return (
-   <div>
-    {isLoading?(<p>Loading...</p>):
-    (
-      data?.map((e:IUsers)=>{
-      return(
-        <>
-        <p>{e.name}</p>
-        <button onClick={()=>deleteUser(e.id)}>Delete</button>
-        </>
-        
-      )
-    })
-  )
-    }
-   </div>
+    <div>
+      
+    </div>
   )
 }
+
+export default HomePage
